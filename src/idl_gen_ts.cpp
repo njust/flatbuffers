1fa6037a85f7e849cf1c5d10e64a49cd99b9229a
Nico Just <mail@njust.de>
2024-01-24 09:26:20 +0100

    Default of int values should be null, otherwise addFieldInt* methodes won't add 0 values

--- src/idl_gen_ts.cpp
+++ src/idl_gen_ts.cpp
@@ -2003,7 +2003,7 @@ class TsGenerator : public BaseGenerator {
           if (IsLong(field.value.type.base_type)) {
             code += "BigInt(0)";
           } else {
-            code += "0";
+            code += "null";
           }
         } else {
           if (field.value.type.base_type == BASE_TYPE_BOOL) { code += "+"; }
